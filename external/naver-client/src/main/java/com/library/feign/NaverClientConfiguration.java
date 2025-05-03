590072b348ec20cdded7225f4963b410c319305f
jyhun007 <jyhun007@naver.com>
2025-05-03 11:52:55 +0900

    [LS-4] 외부 API 연동 - error decoder 적용

--- external/naver-client/src/main/java/com/library/feign/NaverClientConfiguration.java
+++ external/naver-client/src/main/java/com/library/feign/NaverClientConfiguration.java
@@ -1,5 +1,6 @@
 package com.library.feign;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +12,10 @@ public class NaverClientConfiguration {
                                           @Value("${external.naver.headers.client-secret}") String clientSecret){
         return requestTemplate -> requestTemplate.header("X-Naver-Client-Id", clientId)
                                                   .header("X-Naver-Client-Secret", clientSecret);
+    }
+
+    @Bean
+    public NaverErrorDecoder naverErrorDecoder(ObjectMapper objectMapper){
+        return new NaverErrorDecoder(objectMapper);
     }
 }
---
+++ external/naver-client/src/test/groovy/com/library/feign/NaverClientTest.groovy
@@ -0,0 +1,8 @@
+package com.library.feign
+
+import spock.lang.Specification
+
+class NaverClientTest extends Specification {
+    def "Search"() {
+    }
+}
